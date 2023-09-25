c3746690880b6b4645eed116bc5a753a51289213
mugen_champloo <abdurahimmahau@gmail.com>
2023-09-25 17:11:15 +0600

    task_v2

--- applications/task.py
+++ applications/task.py
@@ -1,7 +1,7 @@
 from django.core.mail import send_mail
 
 def send_confirmation_email(email, code):
-    full_link = f'https://hakaton-deeee0082bdc.herokuapp.com/api/v1/account/activate/{code}/'
+    full_link = code
     send_mail(
         'User activation',
         full_link,
