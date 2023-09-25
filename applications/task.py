13fcdc246c2f0dad60577b456629842053681a3f
mugen_champloo <abdurahimmahau@gmail.com>
2023-09-25 17:14:54 +0600

    task_v3

--- applications/task.py
+++ applications/task.py
@@ -1,7 +1,7 @@
 from django.core.mail import send_mail
 
 def send_confirmation_email(email, code):
-    full_link = code
+    full_link = f'Ваш код потврждения, для регистрации: {code}'
     send_mail(
         'User activation',
         full_link,
